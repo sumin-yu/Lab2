9eec65c168cdff79e3e0f74a1fd490b773523dca
daemin <eoalsqkr12@snu.ac.kr>
2023-06-07 03:28:08 +0000

    assignment

--- main.cpp
+++ main.cpp
@@ -6,15 +6,29 @@ using namespace std;
 int N,M;
 vector<int> vec;
 
+
+int lists[9];
+bool check[9] = { 0, };
+ 
 void func(int cnt) {
+    if (cnt == M) {
+        for (int i = 0; i < M; i++) {
+            cout << lists[i] << " ";
+        }
+        cout << endl;
+        return;
+    }
 
-
-
-
-
-
-
+    for (int i = 1; i <= N; i++) {
+        if (!check[i]) {
+            check[i] = true;
+            lists[cnt] = i;
+            func(cnt + 1);
+            check[i] = false;
+        }
+    }
 }
+
 
 int main() {
    cin >> N >> M;
