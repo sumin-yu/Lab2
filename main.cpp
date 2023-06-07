9729d56813e3688d14ae44c2970c938c92552284
jangsehoon <rg7366@snu.ac.kr>
2023-06-07 05:25:33 +0000

    modified main.cpp

--- main.cpp
+++ main.cpp
@@ -1,13 +1,14 @@
-#include<iostream>
-#include<vector>
+#include <iostream>
+#include <vector>
 
 using namespace std;
 
-int N,M;
+int N, M;
 vector<int> vec;
+bool visited[9]; 
 
 void func(int cnt) {
-   if (cnt == M) {  
+    if (cnt == M) {  
         for (int i = 0; i < M; i++) {
             cout << vec[i] << ' ';
         }
@@ -16,15 +17,19 @@ void func(int cnt) {
     }
 
     for (int num = 1; num <= N; num++) {
+        if (visited[num]) continue; 
         if (!vec.empty() && vec.back() > num) continue;  
 
+        visited[num] = true; 
         vec.push_back(num);  
-        func(cnt + 1);  
+        func(cnt + 1);
         vec.pop_back();  
+        visited[num] = false; 
     }
 }
 
 int main() {
-   cin >> N >> M;
-   func(0);
+    cin >> N >> M;
+    func(0);
+    return 0;
 }
