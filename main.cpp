2d2d007c634c6b3d75f402b3492bcd3a16d93a5b
changhoon <changhoon27@snu.ac.kr>
2023-06-07 04:22:13 +0000

    assignment

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
-#include<iostream>
-#include<vector>
+#include <iostream>
+#include <vector>
 
 using namespace std;
 
@@ -7,16 +7,30 @@ int N,M;
 vector<int> vec;
 
 void func(int cnt) {
+    if (cnt == M) {
+        for (int elem : vec)
+            cout << elem << " ";
 
-
-
-
-
-
-
+        cout << endl;
+        return;
+    }
+    
+    else {
+        int begin;
+        if (vec.empty())
+            begin = 1;
+        else
+            begin = vec.back() + 1;
+        
+        for (int i = begin; i <= N; i++) {
+            vec.push_back(i);
+            func(cnt + 1);
+            vec.pop_back();
+        }
+    }
 }
 
 int main() {
-   cin >> N >> M;
-   func(0);
+    cin >> N >> M;
+    func(0);
 }
