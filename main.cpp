5c0c37fd40057747d8efeb3107a233b65c74ba6e
minhyukjang <jason4012@snu.ac.kr>
2023-06-07 04:06:47 +0000

    modify func

--- main.cpp
+++ main.cpp
@@ -1,22 +1,32 @@
-#include<iostream>
-#include<vector>
+#include <iostream>
+#include <vector>
 
 using namespace std;
 
-int N,M;
+int N, M;
 vector<int> vec;
-
-void func(int cnt) {
-
-
-
-
-
-
-
+vector<bool> visited;
+void func(int num, int cnt){//num contains where to start search
+    if(cnt == M){
+        for(int i = 0 ; i< M ;i++){
+            cout<<vec[i]<<" ";
+        }
+        cout<<endl;
+        return;
+    }
+    for(int i = num ; i<=N; i++){
+        if(visited[i]==false){
+            visited[i]=true;
+            vec[cnt] = i;
+            func(i+1, cnt+1);
+            visited[i]=false;
+        }
+    }
 }
 
-int main() {
-   cin >> N >> M;
-   func(0);
+int main(){
+    cin >> N >> M;
+    visited = vector<bool> (N,false);
+    vec = vector<int> (M, 0);
+    func(1, 0);
 }
