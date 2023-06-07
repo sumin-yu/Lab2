b992cb135bd2818fba9329157324e711281ec007
jangsehoon <rg7366@snu.ac.kr>
2023-06-07 03:23:16 +0000

    modified main.cpp

--- main.cpp
+++ main.cpp
@@ -7,13 +7,21 @@ int N,M;
 vector<int> vec;
 
 void func(int cnt) {
+   if (cnt == M) {  
+        for (int i = 0; i < M; i++) {
+            cout << vec[i] << ' ';
+        }
+        cout << '\n';
+        return;
+    }
 
+    for (int num = 1; num <= N; num++) {
+        if (!vec.empty() && vec.back() > num) continue;  
 
-
-
-
-
-
+        vec.push_back(num);  
+        func(cnt + 1);  
+        vec.pop_back();  
+    }
 }
 
 int main() {
