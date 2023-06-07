98a7a5137f59957d734657a79f615a69a0846685
chaewon <cw031001@snu.ac.kr>
2023-06-07 07:42:26 +0000

    modify func

--- main.cpp
+++ main.cpp
@@ -6,18 +6,22 @@ using namespace std;
 int N,M;
 vector<int> vec;
 
-void func(int cnt) {
+void func(int n, int m, int index, vector<int>& vec) {
+    if(vec.size() == m){
+        for(int num : vec) cout << num << " ";
+        cout << endl;
+        return;
+    }
 
-
-
-
-
-
-
+    for(int i = index; i <= n; i++){
+        vec.push_back(i);
+        func(n, m, i + 1, vec);
+        vec.pop_back();
+    }
 }
 
 int main() {
    cin >> N >> M;
-   func(0);
+   func(N, M, 1, vec);
    return 0;
 }
