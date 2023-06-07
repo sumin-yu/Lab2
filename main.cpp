0946462d267527bbb6121ec46acb155ada922b0b
JunKyungSeo <esel343434@snu.ac.kr>
2023-06-07 08:44:12 +0000

    lab2assignment

--- main.cpp
+++ main.cpp
@@ -1,22 +1,29 @@
-#include<iostream>
-#include<vector>
-
+#include <iostream>
+#include <vector>
 using namespace std;
 
 int N,M;
+
 vector<int> vec;
 
 void func(int cnt) {
+    if (vec.size() == M) {
+        for (auto i : vec) {
+            cout << i << " ";
+        }
+        cout << endl;
+        return;
+    }
 
-
-
-
-
-
-
+    for (int i = cnt + 1; i <= N; i++) {
+        vec.push_back(i);
+        func(i);
+        vec.pop_back();
+    }
 }
 
 int main() {
-   cin >> N >> M;
-   func(0);
+    cin >> N >> M;
+    func(0);
+    return 0;
 }
