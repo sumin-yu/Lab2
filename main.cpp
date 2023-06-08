e2acfc3199b807629df240fc76aee4a98389e452
dojinkim <doji2003@snu.ac.kr>
2023-06-08 18:05:23 +0000

    mission complete

--- main.cpp
+++ main.cpp
@@ -7,16 +7,30 @@ int N,M;
 vector<int> vec;
 
 void func(int cnt) {
+    if (cnt == 0) {
+        for (int i = 1; i <= M; i++)
+            vec.push_back(i);
+    }
+    else {
+        int i(0), k(1);
+        while (vec[M - 1 - i] == (N - i)) i++;
+        vec[M - 1 - i] += 1;
+        while (k <= i) {
+            vec[M - 1 - i + k] = vec[M - 1 - i] + k;
+            k++;
+        }
+    }
 
+    for (int elem : vec)
+        cout << elem << ' ';
 
-
-
-
-
-
+    if (vec[0] < (N - M + 1)) {
+        cout << endl;
+        func(++cnt);
+    }
 }
 
 int main() {
-   cin >> N >> M;
-   func(0);
+    cin >> N >> M;
+    func(0);
 }
