0626fc63d1a713fbdd3f21290d32906189c3a707
naemduu <naemduu@gmail.com>
2023-06-09 00:11:11 +0900

    modify func

--- main.cpp
+++ main.cpp
@@ -7,13 +7,31 @@ int N,M;
 vector<int> vec;
 
 void func(int cnt) {
-
-
-
-
-
-
-
+    if(cnt==0){
+        for(int i=1; i<=M; i++){
+            vec.push_back(i);
+        }
+        cnt++;
+    }
+    if(cnt==M+1) {
+        for(int i=N-M+1; i<=N; i++){
+            cout<<i<<" ";
+        }
+        return;
+    }
+    if(vec[M-cnt]==N+1-cnt){
+        func(cnt+1);
+    }else{
+        for(int i=0; i<M; i++){
+            cout<<vec[i]<<" ";
+        }
+        cout<<endl;
+        vec[M-cnt]++;
+        for(int i=M-cnt+1; i<M; i++){
+            vec[i]=vec[i-1]+1;
+        }
+        func(1);
+    }
 }
 
 int main() {
