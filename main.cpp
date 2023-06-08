aa74193209c2b74719e0ba3e3507476abb0b4e53
youngjuchoi <myday05@snu.ac.kr>
2023-06-08 15:37:23 +0000

    modify func

--- main.cpp
+++ main.cpp
@@ -7,16 +7,25 @@ int N,M;
 vector<int> vec;
 
 void func(int cnt) {
+	if (vec.size() == M)
+	{
+		for (auto i:vec)
+		{
+			cout << i << " ";
+		}
+		cout << endl;
+	}
 
-
-
-
-
-
-
+	for (int i=cnt; i<=N; i++)
+	{
+		vec.push_back(i);
+		func(i+1);
+		vec.pop_back();
+	}
 }
 
 int main() {
    cin >> N >> M;
-   func(0);
+   func(1);
+   return 0;
 }
