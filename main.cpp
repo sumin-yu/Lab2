c0fc29d00cb027b89cc5df80999e0544d47b1574
dongyun <se20288@snu.ac.kr>
2023-06-09 09:22:41 +0000

    modify func

--- main.cpp
+++ main.cpp
@@ -8,14 +8,25 @@ vector<int> vec;
 
 void func(int cnt) {
 
-if(cnt<=0)
-cout<<"Hello"<<endl;
-
-
-
-
-
+ if(vec.size()==M){
+    for(int i=0; i<M; i++)
+            {
+                cout<<vec[i]<<" ";
+            }
+        cout<<endl;
+        return;
+        }
+    for(int i=(cnt+1); i<=N; i++)
+    {   
+        
+        vec.push_back(i);
+        func(i);
+        vec.pop_back();
+        
+    }
 }
+
+
 
 int main() {
    cin >> N >> M;
