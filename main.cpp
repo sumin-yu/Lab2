b05fa8e2375972ad1b7c87684fd6f0584f4293c4
dongyun <se20288@snu.ac.kr>
2023-06-09 09:38:48 +0000

    modify func

--- main.cpp
+++ main.cpp
@@ -6,13 +6,22 @@ using namespace std;
 int N,M;
 vector<int> vec;
 
-void func(int cnt) {
-
-
-
-
-
-
+void func(int cnt) { if(vec.size()==M){
+    for(int i=0; i<M; i++)
+            {
+                cout<<vec[i]<<" ";
+            }
+        cout<<endl;
+        return;
+        }
+    for(int i=(cnt+1); i<=N; i++)
+    {   
+        
+        vec.push_back(i);
+        func(i);
+        vec.pop_back();
+        
+    }
 
 }
 
