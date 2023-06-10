e6982032401fcd4f5a29b1ac458109a3aff61d0d
boseongyeom <ybs1923@snu.ac.kr>
2023-06-10 09:54:59 +0000

    Lab2_git_fork_assignment_2020-18424

--- main.cpp
+++ main.cpp
@@ -7,13 +7,30 @@ int N,M;
 vector<int> vec;
 
 void func(int cnt) {
-
-
-
-
-
-
-
+    for(int i=0;i<M;i++){
+        vec.push_back(i+1);
+    }
+    while(1){
+        for(int j=0;j<M;j++){
+	    cout << vec[j] << " ";
+        }
+	cout << endl;
+	if(vec[0]==(N-M+1)){
+	    return;
+	}
+	for(int k=1;k<M;k++){
+	    if(vec[k]==(N-(M-k-1))){
+		vec[k-1]++;
+		for(int a=k;a<M;a++){
+		    vec[a]=vec[a-1]+1;
+		}
+		break;
+	    }
+	    if((k==(M-1))&&(vec[k] != N)){
+		vec[k]++;
+	    }
+	}
+    }
 }
 
 int main() {
