f0f53b174c85f125cef76afe58432749f1a16c77
ponen123 <135787282+ponen123@users.noreply.github.com>
2023-06-13 22:32:26 +0900

    main.cpp

--- main.cpp
+++ main.cpp
@@ -7,13 +7,33 @@ int N,M;
 vector<int> vec;
 
 void func(int cnt) {
-cout << "Hello World" << endl;
+   if(cnt==M){
+        for(int i=0;i<M;i++){
+            cout<<vec[i]<<" ";
+        }
+        cout<<endl;
+        vec.pop_back();
+    }
+    else{
+        if(cnt==0){
 
-
-
-
-
-
+            for(int j=1;j<=N-(M-cnt-1);j++){
+                vec.push_back(j);
+                cnt++;
+                func(cnt);
+                cnt--;
+            }
+        }
+        else{
+            for(int k=vec[cnt-1]+1;k<=N-(M-cnt-1);k++){
+                vec.push_back(k);
+                cnt++;
+                func(cnt);
+                cnt--;
+            }
+            vec.pop_back();
+        }
+    }
 }
 
 int main() {
