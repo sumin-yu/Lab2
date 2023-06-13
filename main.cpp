76afe57e9ca493a59edc545dbce3f2512ef3c80a
wongi <leo0524@snu.ac.kr>
2023-06-13 19:01:34 +0000

    정원기

--- main.cpp
+++ main.cpp
@@ -5,15 +5,22 @@ using namespace std;
 
 int N,M;
 vector<int> vec;
+vector<bool> visited;
 
 void func(int cnt) {
+    if (vec.size() == M) {
+        for(int i = 0; i < M; i++) {
+            cout << vec[i] << " ";
+        }
+        cout << endl;
+        return;
+    }
 
-
-
-
-
-
-
+    for (int i = cnt + 1; i <= N; i++) {
+        vec.push_back(i);
+        func(i);
+        vec.pop_back();
+    }
 }
 
 int main() {
