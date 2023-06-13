ca51d2b09d840b67eb09910f34c2e8d72ff7ecff
chanmin <dreaminalan@snu.ac.kr>
2023-06-14 00:00:31 +0900

    modify func

--- main.cpp
+++ main.cpp
@@ -6,17 +6,30 @@ using namespace std;
 int N,M;
 vector<int> vec;
 
-void func(int cnt) {
+void swap(int i, int j){
+    int num = vec[i];
+    vec[i] = vec[j];
+    vec[j] = num;
+}
 
-
-
-
-
-
-
+void func(int count) {
+    if(count == M){
+        for(int i = 0; i < M; i++){
+            cout << vec[i] << " ";
+        }
+        cout << endl;
+    }
+    else{
+        for(int i = count; i < N; i++){
+            swap(count, i);
+            func(count + 1);
+            swap(count, i);
+        }
+    }
 }
 
 int main() {
    cin >> N >> M;
+   for(int i = 0; i < N; i++) vec.push_back(i + 1);
    func(0);
 }
