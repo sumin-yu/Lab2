9fb68766045d5324a4583797370e16691e5d2263
MinjunChoe <choimj21@snu.ac.kr>
2023-06-13 15:24:29 +0000

    add main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 #include<vector>
+#include<algorithm>
 
 using namespace std;
 
@@ -7,15 +8,27 @@ int N,M;
 vector<int> vec;
 
 void func(int cnt) {
-   while (vec.size() < M){
-      
-      
-      func(cnt);
-   }      
-   
-   if(vec.size() == M)
-      return
+    vector<int> allNum;
+    vector<bool> temp(allNum.size(), false);
 
+    for(int i = 0; i < N; i++)
+	allNum[i] = i + 1;
+
+    for(int i = 0; i < M; i++)
+	temp[i] = true;
+
+    do {
+	for(int i = 0; i < N; i++) {
+	    if(temp[i]) {
+		cout << allNum[i] << ' ';
+		cnt++;
+	    }
+	}
+	cout << endl;
+    } while(prev_permutation(temp.begin(), temp.end));
+
+    cnt = cnt / M;
+    return;
 }
 
 int main() {
