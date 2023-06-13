3d750e673339db0deb2728be81e96e7706edef3f
hyl <hyl0919@snu.ac.kr>
2023-06-13 08:01:06 +0000

    changed the main file

--- main.cpp
+++ main.cpp
@@ -3,20 +3,31 @@
 
 using namespace std;
 
-int N,M;
-vector<int> vec;
+int N, M;
+vector<int> vec(0);
 
 void func(int cnt) {
 
+    if (cnt == M) {
+        for (auto it : vec) {
+            cout << it;
+        }
+        cout << endl;
+        return;
+    }
 
-
-
-
-
-
+    else {
+        for (int i = 1; i < N + 1; i++) {
+            if (vec.empty() || i > vec.back()) {
+                vec.push_back(i);
+                func(cnt + 1);
+                vec.pop_back();
+            }
+        }
+    }
 }
 
 int main() {
-   cin >> N >> M;
-   func(0);
+    cin >> N >> M;
+    func(0);
 }
