cbbc73ad36bb13fd45f36afe0a2051e8f538e69b
MinGGachi <135788702+MinGGachi@users.noreply.github.com>
2023-06-14 00:36:04 +0900

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,34 +4,35 @@
 
 using namespace std;
 
-int N,M;
+int N, M;
 vector<int> vec;
 
 void func(int cnt) {
     vector<int> allNum;
-    vector<bool> temp(allNum.size(), false);
+    vector<bool> temp(N, false);
 
     for(int i = 0; i < N; i++)
-	allNum[i] = i + 1;
+        allNum.push_back(i + 1);
 
     for(int i = 0; i < M; i++)
-	temp[i] = true;
+        temp[i] = true;
 
     do {
-	for(int i = 0; i < N; i++) {
-	    if(temp[i]) {
-		cout << allNum[i] << ' ';
-		cnt++;
-	    }
-	}
-	cout << endl;
-    } while(prev_permutation(temp.begin(), temp.end));
+        for(int i = 0; i < N; i++) {
+            if(temp[i]) {
+                cout << allNum[i] << ' ';
+                cnt++;
+            }
+        }
+        cout << endl;
+    } while(prev_permutation(temp.begin(), temp.end()));
 
     cnt = cnt / M;
     return;
 }
 
 int main() {
-   cin >> N >> M;
-   func(0);
+    cin >> N >> M;
+    func(0);
+    return 0;
 }
