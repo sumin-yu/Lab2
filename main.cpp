b5db46f4d6ec99f83d8899ec737e436e9fc2c618
Jaesuk <kjsbrian@snu.ac.kr>
2023-06-13 04:46:53 +0000

    modify function

--- main.cpp
+++ main.cpp
@@ -7,7 +7,21 @@ int N,M;
 vector<int> vec;
 
 void func(int cnt) {
+if (cnt == M) {
+        for (auto num : vec) {
+            cout << num;
+        }
+        cout << ' ';
+        return;
+    }
 
+    for (int i = 1; i <= N; i++) {
+        if (vec.empty() || i > vec.back()) {
+            vec.push_back(i);
+            func(cnt + 1);
+            vec.pop_back();
+        }
+    }
 
 
 
