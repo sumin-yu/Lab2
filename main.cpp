05427d4290043359667788dd763f0fdf4f5bd860
Ye <1707qwe@snu.ac.kr>
2023-06-13 13:59:05 +0000

    Yejun Kim

--- main.cpp
+++ main.cpp
@@ -5,14 +5,29 @@ using namespace std;
 
 int N,M;
 vector<int> vec;
+int point;
 
 void func(int cnt) {
+    if (cnt == M){
+        for (auto it : vec)
+            cout << it << ' ';
+        
+        cout << endl;
+        return;
+    }
 
+    if (vec.empty()){
+        point = 1;
+    }
+    else{
+        point = vec.back()+1;
+    }
 
-
-
-
-
+    for (int ind=point; ind<=N; ind++){
+        vec.push_back(ind);
+        func(cnt+1);
+        vec.pop_back();
+    }
 
 }
 
