25b2c4046d5540b8bdbd68f45333fbc0aa0e11bf
lee <k.snu.ac.kr>
2023-06-13 13:09:49 +0000

    modify func

--- main.cpp
+++ main.cpp
@@ -1,26 +1,31 @@
 #include<iostream>
 #include<vector>
-#include<map>
 using namespace std;
 
-void func(int N, int M) {
-    map<int,int> list;
-    for(int i=0; i<M; i++){
-        while(1){
-            int n=rand()%N;
-            if(list.count(n)==0){  
-                list.insert({n,1});
-                break;
-            }
-        }
+void func(vector<int> list, vector<int> pri, int M, int a, int b)
+{
+    if (M == 0){
+        for(int i = 0; i < pri.size(); i++)
+            cout << pri[i] << " ";
+        cout << endl;
     }
-    for(auto i : list){
-        cout<<i.first<<" ";
+    else if (b == list.size()){
+        return;
+    }
+    else{
+        pri[a] = list[b];
+        func(list, pri, M - 1, a + 1, b + 1);
+        func(list, pri, M, a, b + 1);
     }
 }
 
 int main() {
     int N,M;
     cin >> N >> M;
-    func(N,M);
+        vector<int> list;
+    for(int i=1; i<N+1; i++){
+        list.push_back(i);
+    }
+    vector<int> pri(M);
+    func(list, pri,M, 0, 0);
 }
