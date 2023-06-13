18c6e9e2e2e2e2f75a6b229b8e3d61ea7cba98cf
Doyoung <cathy2653@snu.ac.kr>
2023-06-14 01:59:52 +0900

    🎸 [Add] Backtracking

--- main.cpp
+++ main.cpp
@@ -3,20 +3,27 @@
 
 using namespace std;
 
-int N,M;
+int N, M;
 vector<int> vec;
+int start = 1;
 
 void func(int cnt) {
-
-
-
-
-
-
-
+    if (cnt == M) {
+        for (int i = 0; i < M; i++) 
+            cout << vec[i] << " ";
+        cout << endl;
+        return;
+    }
+    for (int i = start; i <= N; i++) {
+        vec[cnt] = i;
+        start = i + 1;
+        func(cnt + 1);
+        start = i; 
+    }
 }
 
 int main() {
-   cin >> N >> M;
-   func(0);
+    cin >> N >> M;
+    vec.resize(M);
+    func(0);
 }
