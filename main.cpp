aa8e1bcdd037bc062d6d58c09feb477b717e9959
suhwanCha <cha3373@snu.ac.kr>
2023-06-13 09:19:47 +0000

    modify func

--- main.cpp
+++ main.cpp
@@ -7,16 +7,31 @@ int N,M;
 vector<int> vec;
 
 void func(int cnt) {
-
-
-
-
-
-
-
+    vector<int> vec_dup = vec;
+    if(vec.size() == M)
+    {
+        for(auto it = vec.begin(); it!=vec.end(); it++)
+        {
+            cout << *it << " " ;
+        }
+        cout << endl;
+        return;
+    }
+    
+    if(cnt > N)
+        return;
+    
+    for(int i = cnt + 1; i <= N; i++)
+    {
+        vec = vec_dup;
+        vec.push_back(i);
+        func(i);
+    }
 }
 
 int main() {
    cin >> N >> M;
    func(0);
 }
+
+
