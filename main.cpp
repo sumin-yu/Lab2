b6b213a242ac2676d1fad2997786644229fbce16
jong <cepheid6496@snu.ac.kr>
2023-06-13 20:59:39 +0000

    2019-14031 parkjonghyeon

--- main.cpp
+++ main.cpp
@@ -1,22 +1,33 @@
-#include<iostream>
-#include<vector>
+#include <iostream>
+#include <vector>
 
 using namespace std;
 
-int N,M;
-vector<int> vec;
+int N, M;
+vector<int> v;
 
-void func(int cnt) {
+void f(int cnt, int start) {
+    
+    if (cnt == M) {
+        for (int i=0;i<v.size();i++) {
+            cout << v[i] << ' ';
+        }
+        cout << '\n';
+        return;
+    }
 
-
-
-
-
-
-
+    
+    for (int num = start; num <= N; num++) {
+        v.push_back(num);
+        f(cnt + 1, num + 1);        
+        v.pop_back();
+    }
 }
 
 int main() {
-   cin >> N >> M;
-   func(0);
+    cin >> N >> M;
+
+    f(0, 1);
+
+    return 0;
 }
