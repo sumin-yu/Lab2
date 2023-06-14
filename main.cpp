41f13a7467613cada86670bd35d9f66bcd8d364d
chanyoung123 <87213295+chanyoung123@users.noreply.github.com>
2023-06-14 09:35:36 +0900

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,22 +1,31 @@
-#include<iostream>
-#include<vector>
+#include <iostream>
+#include <vector>
 
 using namespace std;
 
-int N,M;
+int N, M;
 vector<int> vec;
 
 void func(int cnt) {
-
-
-
-
-
-
-
+    if (cnt == M) {
+        for (int num : vec) {
+            cout << num << " ";
+        }
+        cout << endl;
+        return;
+    }
+    int start = (vec.empty() ? 1 : vec.back() + 1); 
+    for (int i = start; i <= N; i++) {
+        vec.push_back(i);
+        func(cnt + 1);
+        vec.pop_back();
+    }
 }
 
 int main() {
-   cin >> N >> M;
-   func(0);
+    cin >> N >> M;
+
+    func(0);
+
+    return 0;
 }
